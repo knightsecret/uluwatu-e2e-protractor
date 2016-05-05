d9bd0faa5e8ce43cddccbbfa018ab6813138df8e
Annamaria Szegedi <aszegedi@hortonworks.com>
2016-05-05 14:04:16 +0200

    Add comment to the cluster create testing

--- tests/LoginSpec.js
+++ tests/LoginSpec.js
@@ -4,18 +4,18 @@ var DashboardPage = require('../pages/DashboardPage.js');
 
 describe('Testing Cloudbreak login', function () {
   var dashboardPage;
-  var preprodURL = 'https://pre-prod-cloudbreak.sequenceiq.com/#/';
+  var cloudbreakURL = 'cloudbreak.sequenceiq.com/#/';
 
   describe('with ' + process.env.USERNAME + ' user', function () {
     dashboardPage = new DashboardPage();
 
     it('should be at the Dashboard page', function () {
-      expect(browser.getCurrentUrl()).toEqual(preprodURL);
+      expect(browser.getCurrentUrl()).toContain(cloudbreakURL);
     });
 
     it('should see default blueprints', function () {
       dashboardPage.getBadgeValue(3).then(function (value) {
-        expect(value).toEqual(3);
+        expect(value).toBeGreaterThan(2);
       });
     });
 
