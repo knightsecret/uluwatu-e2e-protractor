0e00d060c9d78ab30a9e8fcd72e587a8b72be8af
Annamaria Szegedi <aszegedi@hortonworks.com>
2016-06-14 21:43:23 +0200

    Experimenting with conditional test cases

--- tests/ClusterSpec.js
+++ tests/ClusterSpec.js
@@ -12,8 +12,7 @@ describe('Testing cluster creation', function () {
   var regionName = 'EU (Ireland)';
   var networkName = 'default-aws-network';
   var securityGroup = 'all-services-port';
-  var specResult;
-  var isClusterUp = true;
+  var isClusterUp;
 
   describe('on a new AWS cluster', function () {
       basePage = new BasePage();
@@ -24,60 +23,38 @@ describe('Testing cluster creation', function () {
           basePage.selectCredentialByName(credentialName);
       });
       afterAll(function() {
-          console.log(specResult.result.status);
-          console.log(isClusterUp);
-
           console.log('Test suit teardown has started!');
           dashboardPage.deleteBlueprint(blueprintName);
           dashboardPage.deleteAWSCredential(credentialName);
       });
-      specResult = it('Cluster should be launched', function (done) {
+
+      it('Cluster should be installed', function (done) {
           expect(basePage.getSelectedCredential()).toEqual(credentialName);
           expect(basePage.createNewAWSCluster(clusterName, regionName, networkName, securityGroup, blueprintName)).toBeTruthy();
           expect(basePage.isClusterInstalling()).toBeTruthy();
-
+          // 'Cluster should be launched'
           jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60000;
           basePage.isClusterInstalled().then(function(result) {
               return isClusterUp = result;
           });
-
           expect(isClusterUp).toBeTruthy();
+
+          if(isClusterUp) {
+              // 'Cluster Details should be available'
+              expect(basePage.isClusterDetailsControllers(clusterName)).toBeTruthy();
+              // 'Cluster AutoScaling should be available'
+              expect(basePage.isAmbariAutoScalingAvailable(clusterName)).toBeTruthy();
+              // 'Cluster should be stopped'
+              expect(basePage.stopCluster(clusterName)).toBeTruthy();
+              expect(basePage.isClusterStopped()).toBeTruthy();
+              // 'Cluster should be started'
+              expect(basePage.startCluster(clusterName)).toBeTruthy();
+              expect(basePage.isClusterStarted()).toBeTruthy();
+              // 'Cluster should be terminated'
+              expect(basePage.terminateCluster(clusterName)).toBeTruthy();
+              expect(basePage.isClusterRemoved()).toBeTruthy();
+          }
           done();
       }, 40 * 60000);
-      if(isClusterUp) {
-          it('Cluster Details should be available', function () {
-              expect(basePage.isClusterDetailsControllers(clusterName)).toBeTruthy();
-          });
-          it('Cluster AutoScaling should be available', function () {
-              expect(basePage.isAmbariAutoScalingAvailable(clusterName)).toBeTruthy();
-          });
-          it('Cluster should be stopped', function (done) {
-              expect(basePage.stopCluster(clusterName)).toBeTruthy();
-
-              jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60000;
-              expect(basePage.isClusterStopped()).toBeTruthy();
-              done();
-          }, 40 * 60000);
-          it('Cluster should be started', function (done) {
-              expect(basePage.startCluster(clusterName)).toBeTruthy();
-
-              jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60000;
-              expect(basePage.isClusterStarted()).toBeTruthy();
-              done();
-          }, 40 * 60000);
-          it('Cluster should be terminated', function (done) {
-              expect(basePage.terminateCluster(clusterName)).toBeTruthy();
-
-              jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60000;
-              expect(basePage.isClusterRemoved()).toBeTruthy();
-              done();
-          }, 40 * 60000);
-      } else {
-          xit('Cluster Details should be available', function () {});
-          xit('Cluster AutoScaling should be available', function () {});
-          xit('Cluster should be stopped', function () {});
-          xit('Cluster should be started', function () {});
-          xit('Cluster should be terminated', function () {});
-      }
   });
 });
